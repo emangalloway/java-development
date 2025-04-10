49461890841f2899bd6af834e138bbe4efabdc0c
emangalloway <emanuelgalloway25@gmail.com>
2025-04-10 09:27:49 -0400

    Initial upload

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone! My name is Emanuel, and I love Cinnamon Toast Crunch");
+    }
+}
---
+++ workbook-1/shopping-list-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Water");
+        System.out.println("Beef");
+        System.out.println("Eggs");
+        System.out.println("Apple");
+        System.out.println("Watermelon");
+        System.out.println("Sushi");
+        System.out.println("Bread");
+        System.out.println("Chip");
+        System.out.println("Ceral");
+        System.out.println("Spinach");
+    }
+}
