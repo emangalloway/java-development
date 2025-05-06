42160a78de2ff20f5421c18a81909c514f5648d2
emangalloway <emanuelgalloway25@gmail.com>
2025-05-06 11:07:49 -0400

    Modified Operations

---
+++ workbook-4/HotelOperationsModified/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = false;
+        this.isDirty = false;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return !isOccupied && !isDirty;
+    }
+
+    //checkIn
+    public void checkIn() {
+        if (!isOccupied && !isDirty) {
+            isOccupied = true;
+            isDirty = true;
+        }
+    }
+
+    //checkOut
+    public void checkOut() {
+        if (!isOccupied && isDirty) {
+            isOccupied = true;
+        }
+    }
+
+    //cleanRoom
+    public void cleanRoom(){
+        if (!isOccupied) {
+            isDirty = false;
+        }
+    }
+}
